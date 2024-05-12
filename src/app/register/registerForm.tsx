98bc83039bffa9e8fafd1e4d3b2e9bd1444fca31
ssisaias <ssisaias@users.noreply.github.com>
2024-05-12 18:50:39 -0300

    add form validation to the register form

--- src/app/register/registerForm.tsx
+++ src/app/register/registerForm.tsx
@@ -6,9 +6,39 @@ import { Heading } from '@/components/Heading'
 import { Multistep } from '@/components/Multistep'
 import { Text } from '@/components/Text'
 import { TextInput } from '@/components/TextInput'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, 'O nome de usuário deve ter pelo menos 3 caracteres')
+    .max(20, 'O nome de usuário deve ter no máximo 20 caracteres')
+    .regex(
+      /^[a-z0-9-]+$/,
+      'O nome de usuário deve conter apenas letras, números e hífens',
+    )
+    .transform((value) => value.toLowerCase()),
+  name: z.string().min(3, 'O nome deve ter pelo menos 3 caracteres'),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
 
 export function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data)
+  }
+
   return (
     <>
       <Heading as={'strong'} className="leading-base">
@@ -21,18 +51,41 @@ export function RegisterForm() {
 
       <Multistep size={4} currentStep={1} />
 
-      <Box id="RegisterForm" as={'form'} className="mt-6 flex flex-col gap-4">
+      <Box
+        id="RegisterForm"
+        as={'form'}
+        className="mt-6 flex flex-col gap-4"
+        onSubmit={handleSubmit(handleRegister)}
+      >
         <label className="flex flex-col gap-2">
           <Text size="sm">Nome de usuário</Text>
-          <TextInput prefix="sample.com/" placeholder="seu-usuario"></TextInput>
+          <TextInput
+            prefix="sample.com/"
+            placeholder="seu-usuario"
+            {...register('username')}
+          ></TextInput>
+          {errors.username && (
+            <Text size="sm" className="text-descructive-red">
+              {errors.username.message}
+            </Text>
+          )}
         </label>
 
         <label className="flex flex-col gap-2">
           <Text size="sm">Nome Completo</Text>
-          <TextInput placeholder="Seu nome"></TextInput>
+          <TextInput placeholder="Seu nome" {...register('name')}></TextInput>
+          {errors.name && (
+            <Text size="sm" className="text-descructive-red">
+              {errors.name.message}
+            </Text>
+          )}
         </label>
 
-        <Button type="submit" className="self-end w-full">
+        <Button
+          type="submit"
+          className="self-end w-full"
+          disabled={isSubmitting}
+        >
           Próximo passo <ArrowRight />
         </Button>
       </Box>
