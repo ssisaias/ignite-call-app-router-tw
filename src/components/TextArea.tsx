d01ad7259ab2f7b93253876c5373f395050a76ba
ssisaias <ssisaias@users.noreply.github.com>
2024-05-25 17:08:57 -0300

    TextArea: For react hook form to work in our component need to forward the ref to it

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,20 +1,28 @@
 'use client'
+
+import { forwardRef } from 'react'
+
 export interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-export function TextArea(props: TextAreaProps) {
-  return (
-    <>
-      <textarea
-        className={`
-        box-border min-h-24 w-96 resize-y rounded-sm border-2 border-solid border-gray-900 bg-gray-900 px-3
-        py-4 font-default text-sm font-regular text-white
-        placeholder:text-gray-400 focus-within:border-ignite300
-        focus-within:outline-none disabled:cursor-not-allowed disabled:opacity-50 
-      `}
-        placeholder={props.placeholder}
-        disabled={props.disabled}
-      ></textarea>
-    </>
-  )
-}
+// eslint-disable-next-line react/display-name
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ ...props }: TextAreaProps, ref) => {
+    return (
+      <>
+        <textarea
+          className={`
+          box-border min-h-24 w-96 resize-y rounded-sm border-2 border-solid border-gray-900 bg-gray-900 px-3
+          py-4 font-default text-sm font-regular text-white
+          placeholder:text-gray-400 focus-within:border-ignite300
+          focus-within:outline-none disabled:cursor-not-allowed disabled:opacity-50 
+        `}
+          placeholder={props.placeholder}
+          disabled={props.disabled}
+          ref={ref}
+          {...props}
+        ></textarea>
+      </>
+    )
+  },
+)
