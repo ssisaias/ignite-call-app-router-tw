8fa59e1ec34114bc18a559ee3236364c9e84782c
ssisaias <ssisaias@users.noreply.github.com>
2024-05-25 17:10:58 -0300

    Api: adds update profile bio route

---
+++ src/app/api/v1/users/profile/route.ts
@@ -0,0 +1,24 @@
+import { auth } from '@/lib/auth'
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+
+const updateProfileBodySchema = z.object({
+  bio: z.string(),
+})
+
+export async function PUT(req: NextRequest) {
+  const session = await auth()
+
+  if (!session?.user) return NextResponse.json(null, { status: 401 })
+
+  const reqBody = await req.json()
+  const { bio } = updateProfileBodySchema.parse(reqBody)
+
+  await prisma.user.update({
+    where: { id: session.user.id },
+    data: { bio },
+  })
+
+  return new NextResponse(null, { status: 204 })
+}
--- src/app/api/v1/users/time-intervals/route.ts
+++ src/app/api/v1/users/time-intervals/route.ts
@@ -30,5 +30,5 @@ export async function POST(req: NextRequest) {
     })),
   })
 
-  return NextResponse.json(null, { status: 201 })
+  return new NextResponse(null, { status: 201 })
 }
