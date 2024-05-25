4a013ed15aa954c375816b1087280dc9efde9d57
ssisaias <ssisaias@users.noreply.github.com>
2024-05-25 00:24:16 -0300

    Persist the intervals data

--- src/app/api/v1/users/time-intervals/route.ts
+++ src/app/api/v1/users/time-intervals/route.ts
@@ -1,10 +1,34 @@
 import { auth } from '@/lib/auth'
-import { NextResponse } from 'next/server'
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
 
-export async function POST() {
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeMinutes: z.number(),
+      endTimeMinutes: z.number(),
+    }),
+  ),
+})
+
+export async function POST(req: NextRequest) {
   const session = await auth()
 
-  if (!session?.user) return null
+  if (!session?.user) return NextResponse.json(null, { status: 401 })
 
-  return NextResponse.json(session)
+  const reqBody = await req.json()
+  const { intervals } = timeIntervalsBodySchema.parse(reqBody)
+
+  await prisma.userTimeInterval.createMany({
+    data: intervals.map((interval) => ({
+      user_id: session.user!.id!,
+      week_day: interval.weekDay,
+      time_start_in_minutes: interval.startTimeMinutes,
+      time_end_in_minutes: interval.endTimeMinutes,
+    })),
+  })
+
+  return NextResponse.json(null, { status: 201 })
 }
