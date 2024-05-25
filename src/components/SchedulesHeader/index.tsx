eca8e58e47be23e5c15d619f35b9a9cdb7bc0742
ssisaias <ssisaias@users.noreply.github.com>
2024-05-25 20:16:54 -0300

    Fetch the userdata on the public schedules page

--- src/components/SchedulesHeader/index.tsx
+++ src/components/SchedulesHeader/index.tsx
@@ -1,21 +1,54 @@
-import { Avatar } from '../Avatar'
+'use client'
+
+import { getPublicUserInfo } from '@/lib/actions/get-user-calendar-action'
 import { Heading } from '../Heading'
 import { Text } from '../Text'
+import { usePathname } from 'next/navigation'
+import { Avatar } from '../Avatar'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+
+interface UserInfo {
+  id: string
+  username: string
+  name: string
+  bio: string | null
+  avatar_url: string | null
+}
 
 export default function SchedulesHeader() {
+  const path = usePathname()
+  const [userInfo, setUserInfo] = useState<null | UserInfo>(null)
+  const username = path.replace('/schedule/', '')
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const data = await getPublicUserInfo(username)
+      setUserInfo(data)
+    }
+    fetchUserInfo()
+  }, [username])
+
   return (
     <div
       id="userHeader"
       className="mt-12 w-full flex flex-col items-center justify-center"
     >
       <div>
-        <Avatar src="https://github.com/ssisaias.png" />
+        {userInfo ? (
+          <Avatar src={userInfo.avatar_url || ''} />
+        ) : (
+          <Avatar src="https://thispersondoesnotexist.com/" />
+        )}
       </div>
       <Heading className="leading-base font-bold" as={'h1'} size="2xl">
-        Isaias Silva
+        {userInfo?.name}
       </Heading>
       <Text className="text-gray200" size="md">
-        Nobody
+        {userInfo?.bio || (
+          <Link href="https://thispersondoesnotexist.com/">
+            you do not exist
+          </Link>
+        )}
       </Text>
     </div>
   )
