a786200ee6ed89cc88e0bcd6cb35d0627ddecfcb
ssisaias <ssisaias@users.noreply.github.com>
2024-05-25 21:46:38 -0300

    user Calendar page

--- src/app/schedule/[username]/calendar/page.tsx
+++ src/app/schedule/[username]/calendar/page.tsx
@@ -1,7 +1,14 @@
+import { Box } from '@/components/Box'
+import { Calendar } from '@/components/Calendar'
+
 export default function CalendarStep() {
   return (
-    <div>
-      <h1>Schedule Form</h1>
-    </div>
+    <Box
+      id="Container"
+      as={'div'}
+      className="mt-6 mr-auto mb-0 p-0 grid max-w-[100%] relative"
+    >
+      <Calendar />
+    </Box>
   )
 }
--- src/lib/utils/get-week-days.ts
+++ src/lib/utils/get-week-days.ts
@@ -1,8 +1,15 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(2021, 5, day)))
     .map((weekday) => {
+      if (short) {
+        return weekday.slice(0, 3)
+      }
+
       return weekday.charAt(0).toUpperCase() + weekday.slice(1)
     })
 }
