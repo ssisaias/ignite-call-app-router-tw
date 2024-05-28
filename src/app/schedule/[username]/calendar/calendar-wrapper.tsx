d260a14b7eca83200e771e9760e463fcd1f11e49
ssisaias <ssisaias@users.noreply.github.com>
2024-05-28 11:55:45 -0300

    Loads the user availability from the api

--- src/app/schedule/[username]/calendar/calendar-wrapper.tsx
+++ src/app/schedule/[username]/calendar/calendar-wrapper.tsx
@@ -2,12 +2,19 @@
 import { Box } from '@/components/Box'
 import { Calendar } from '@/components/Calendar'
 import TimePicker from '@/components/CalendarTimePicker'
+import { api } from '@/lib/axios'
 import clsx from 'clsx'
 import dayjs from 'dayjs'
-import { useState } from 'react'
 
-export default function CalendarWrapper() {
+import { useEffect, useState } from 'react'
+
+export default function CalendarWrapper({
+  username,
+}: {
+  username?: string | null
+}) {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState(null)
 
   const isDateSelected = !!selectedDate
 
@@ -20,6 +27,21 @@ export default function CalendarWrapper() {
     'grid grid-cols-1 md:grid-cols-[1fr,280px] w-7/12': isDateSelected,
     'grid grid-cols-1 w-4/12': !isDateSelected,
   })
+
+  useEffect(() => {
+    if (!selectedDate || !username) {
+      return
+    }
+    api
+      .get(`/users/${username}/agenda`, {
+        params: {
+          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+        },
+      })
+      .then((res) => {
+        console.log(res.data)
+      })
+  }, [selectedDate, username])
 
   return (
     <Box
