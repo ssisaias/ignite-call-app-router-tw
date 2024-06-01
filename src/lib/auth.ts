ed225a92cfacefc398711bf0ca8e980c2a5d0e23
ssisaias <ssisaias@users.noreply.github.com>
2024-06-01 17:17:11 -0300

    Fill the refresh token from google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,6 +9,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth((req) => ({
     google({
       authorization: {
         params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
           scope:
             'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
         },
