ec6ddb390c41fd2fd4fc48c6d7b6284c4be18523
ssisaias <ssisaias@users.noreply.github.com>
2024-06-02 16:29:01 -0300

    try the react cache

--- src/lib/actions/get-user-info.ts
+++ src/lib/actions/get-user-info.ts
@@ -1,20 +1,19 @@
 'use server'
 
 import { eq } from 'drizzle-orm'
+import { cache } from 'react'
 import z from 'zod'
 
 import { dz } from '../drizzle'
 import { users } from '../dz/migrations/schema'
-// import { prisma } from '../prisma'
 import { actionClient } from '../safe-action'
 
 const schema = z.object({
   username: z.string(),
 })
 
-export const getPublicUserInfo = actionClient
-  .schema(schema)
-  .action(async ({ parsedInput: { username } }) => {
+export const getPublicUserInfo = actionClient.schema(schema).action(
+  cache(async ({ parsedInput: { username } }) => {
     return (
       await dz
         .select({
@@ -27,4 +26,5 @@ export const getPublicUserInfo = actionClient
         .where(eq(users.username, username))
         .limit(1)
     )[0]
-  })
+  }),
+)
