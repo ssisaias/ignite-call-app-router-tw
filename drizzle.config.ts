f571e31472c80a0f746fc8fea9f542a2ab70c983
ssisaias <ssisaias@users.noreply.github.com>
2024-06-02 16:29:45 -0300

    drizzle dev x prod config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'drizzle-kit'
 
+const mode = process.env.MODE || 'dev'
+const dbUrl = process.env.TURSO_DATABASE_URL ?? ''
+const authToken = process.env.TURSO_AUTH_TOKEN ?? ''
+
+const credentials = {
+  url: mode === 'dev' ? './prisma/dev.db' : dbUrl,
+  authToken: mode === 'dev' ? '' : authToken,
+}
+
 export default defineConfig({
   dialect: 'sqlite',
   schema: './src/lib/dz/migrations/schema.ts',
   out: './src/lib/dz/migrations',
-  dbCredentials: {
-    url: './prisma/dev.db',
-  },
+  dbCredentials: credentials,
   // Print all statements
   verbose: true,
   // Always ask for confirmation
